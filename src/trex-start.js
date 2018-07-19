eb74b1838c596de81a9455f6ee32c440bb594438
Heyder Dias <heyderpd@gmail.com>
2018-07-18 22:47:24 -0300

    add mp3 and aquidauana bundle

--- src/trex-start.js
+++ src/trex-start.js
@@ -4,7 +4,7 @@
       window.DocumentInjectionWithFetch(
         'https://raw.githubusercontent.com/heyderpd/TRexRunner/master/',
         // 'http://127.0.0.1:8080/',
-        ['robot.js', 'index.html'])
+        ['aquidauana.bundle.js', 'robot.js', 'index.html'])
     },
     200)
 
